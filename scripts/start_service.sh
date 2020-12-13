158b9d78809e925515824d910150ec13a0df907b
Danylo Iesaulov <d.iesaulov@celonis.com>
2020-12-13 19:57:12 +0100

    * Stop the server before starting

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 APP_HOME=/home/ubuntu/mybureau-api
+bash "${APP_HOME}/bin/stop_service.sh"
 nohup java -Dlogging.config="${APP_HOME}/conf/logback.xml" -jar "${APP_HOME}/bin/mybureau-api-0.0.1-SNAPSHOT.jar" --spring.config.location="${APP_HOME}/conf/application.properties" >/dev/null 2>&1 & echo "$!" > "${APP_HOME}/run/.pid"
